4826ab24042a93a9a219b90ccdc177ba4bfa6d91
thecoder.live <82015917+thecoderlive@users.noreply.github.com>
2023-10-11 08:35:30 +0530

    Initial Commit for file: DATA.js

---
+++ DATA.js
@@ -0,0 +1,75 @@
+export const item = {
+    "id": "03a34cce-788a-4ed2-9d84-63a70d414125",
+    "intro": "turn",
+    "image_feed": [
+        {
+            "id": "7a547896-bdce-4849-9a50-efa69578c8a5",
+            "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/140.png"
+        },
+        {
+            "id": "c631a2a3-4a94-4c3b-affa-7f11555f0761",
+            "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/66.png"
+        },
+        {
+            "id": "55e4867d-a9ab-4013-88d0-f54ec139f736",
+            "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/284.png"
+        },
+        {
+            "id": "c6367a6a-3698-4c28-aaf8-e84114ba747d",
+            "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/94.png"
+        },
+        {
+            "id": "835dd866-4594-4343-a685-e4b4551c53be",
+            "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/342.png"
+        }
+    ],
+    "buttons": [
+        {
+            "id": "ab254776-f873-4712-8cde-d2de9026858a"
+        },
+        {
+            "id": "818e7add-77f6-45ca-a5d2-37e1c2e8ae90"
+        },
+        {
+            "id": "cae5ea0b-2525-4d5b-9e2c-e96030f001b1"
+        },
+        {
+            "id": "5b8a1309-bd19-4acf-b100-4e96cf6c5168"
+        },
+        {
+            "id": "e66475b1-4b91-4fd2-8f38-e8d73ba39595"
+        }
+    ],
+    "art_feed": [
+        {
+            "id": "c44a18d7-8858-4388-986c-f3bc863696f5",
+            "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/150.png",
+            "art_caption": "wait",
+            "location": "skin"
+        },
+        {
+            "id": "072550f1-f490-4261-b541-c4cf54fdebe1",
+            "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/306.png",
+            "art_caption": "call",
+            "location": "away"
+        },
+        {
+            "id": "59a76659-9e9a-4a34-8a9e-337e13ffd138",
+            "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/84.png",
+            "art_caption": "thing",
+            "location": "arrive"
+        },
+        {
+            "id": "d07aabfb-d47c-4c38-b41c-d89ff6b1d0a6",
+            "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/273.png",
+            "art_caption": "here",
+            "location": "because"
+        },
+        {
+            "id": "6f3abc38-3e48-416b-8736-8938874598d8",
+            "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/53.png",
+            "art_caption": "left",
+            "location": "test"
+        }
+    ]
+}
